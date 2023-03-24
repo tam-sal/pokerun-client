d03c608acc22170eac078de02d619dee0e6ce7f9
tam-sal <t.saleh.gc@gmail.com>
2023-03-24 11:23:30 -0300

    Client ready to deply

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,7 +2,6 @@ import Card from './Card/Card.jsx'
 import Cards from './Cards/Cards.jsx'
 import Footer from './Footer/Footer.jsx'
 import Header from './Header/Header.jsx'
-import Loading from './Loading/Loading.jsx'
 import Nav from './Nav/Nav.jsx'
 import SearchBar from './SearchBar/SearchBar.jsx'
 import Pagination from './Pagination/Pagination.jsx'
@@ -16,7 +15,6 @@ export {
   Cards,
   Footer,
   Header,
-  Loading,
   Nav,
   SearchBar,
   Pagination,
